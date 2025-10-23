fd76f54c6c737e65916f76080fecf09326f41d7a
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-23 10:10:52 +0000

    test

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,27 @@
 
+let bird;
+let background, birdImg;
+
+function preload(){
+    birdImg=loadImage('assets/bird.png');
+    background=loadImage('assets/background.png')
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+}
+
+function setup(){
+    new Canvas(400, 600);
+    
+    bird = new Sprite(birdImg,200,300,34,24)
+    // write code to create sprite and load bird image on sprite
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+
+    
+
+
+
+}
+
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,24 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y=10;
+    
+    // code to create a new square sprite
+    square=new Sprite(200,300,30,30);
+    square.velocity.y=1
+    
+}
+
+function draw(){
+    
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+    if (mouse.presses()){
+        square.velocity.y=-5
+    }
+}
